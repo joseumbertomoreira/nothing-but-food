f6698dd183953aa2c8eb2c62efd1d53e5f1ac060
José Umberto Moreira Júnior <joseumbertomoreira@gmail.com>
2021-10-28 18:45:38 -0300

    Alimentação da página index por coleções do banco AlimentosTech

--- src/controller/menu.js
+++ src/controller/menu.js
@@ -1,9 +1,37 @@
+const { MongoClient } = require("mongodb");
+
+const uri = 'mongodb://localhost:27017'
+const client = new MongoClient(uri);
+
+
 module.exports = {
 
-  index: (req, res, next) => {
+  index: async (req, res, next) => {
+
+    try {
+      // Conexao do cliente no banco
+      await client.connect();
+
+      // Instancia do banco de dados AlimentosTech
+      let db = await client.db("AlimentosTech"); 
+      
+      
+      // Obtendo instancia da coleção 'Clientes'
+      let collection = db.collection("Clientes");
+
+
+      // Aqui eu pego todos os documentos Clientes e passo pra um array
+      let result = await collection.find({}).toArray()
+
+      res.render('index', { clientes: result })
+
+    } finally {
+      // Fechando a conexao
+      await client.close();
+    }
     
-    // Logica de negocio
-    res.render('index')
+    
+    
 
   }
 
